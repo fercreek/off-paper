f1f7d8cebdf0bc69a982a5bff4f76ac08fdcfd97
Fernando Contreras <fercreek@gmail.com>
2016-05-16 01:57:12 -0500

    Create person endpoint, remove token

--- server/config/express.js
+++ server/config/express.js
@@ -49,20 +49,21 @@ export default function(app) {
    * Lusca - express server security
    * https://github.com/krakenjs/lusca
    */
-  if (env !== 'test' && !process.env.SAUCE_USERNAME) {
-    app.use(lusca({
-      csrf: {
-        angular: true
-      },
-      xframe: 'SAMEORIGIN',
-      hsts: {
-        maxAge: 31536000, //1 year, in seconds
-        includeSubDomains: true,
-        preload: true
-      },
-      xssProtection: true
-    }));
-  }
+   // NOTE Token disabled
+  // if (env !== 'test' && !process.env.SAUCE_USERNAME) {
+  //   app.use(lusca({
+  //     csrf: {
+  //       angular: true
+  //     },
+  //     xframe: 'SAMEORIGIN',
+  //     hsts: {
+  //       maxAge: 31536000, //1 year, in seconds
+  //       includeSubDomains: true,
+  //       preload: true
+  //     },
+  //     xssProtection: true
+  //   }));
+  // }
 
   app.set('appPath', path.join(config.root, 'client'));
 
